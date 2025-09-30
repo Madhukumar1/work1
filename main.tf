59ed4de568144e848d7edf38e7767a3362b75377
Madhukumar1 <madhukumar2k@gmail.com>
2025-09-30 19:17:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_vpc" "eks_vpc" {
 }
 
 # Create Subnets
-resource "aws-subnet" "eks-subnet" {
+resource "aws_subnet" "eks-subnet" {
   count = 2
   vpc_id = aws_vpc.eks_vpc.id
   cidr_block = cidrsubnet(aws_vpc.eks_vpc.cidr_block, 8, count.index)
